2979f9cc414c1c80e192279c991768ae00ac1935
Matt Toseland <m.toseland@validus-ivc.co.uk>
2014-11-03 09:59:48 +0000

    first commit

---
+++ chapter2/app.js
@@ -0,0 +1,9 @@
+(function(){
+  var gem = { name: 'Azurite', price: 2.95 };
+  var app = angular.module('gemStore', []);
+  
+ app.controller('StoreController', function($scope) {
+   this.product = gem;
+ });
+  
+})();
